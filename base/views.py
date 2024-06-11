89cfe28b18800f9ad9abd401d51fd4471d7e4149
ajaypatel <patel.ajay745@gmail.com>
2024-06-11 08:52:47 -0400

    implement room model

--- base/views.py
+++ base/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    
+    context={'rooms':Room.objects.all}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request,pk):
+    room=None
+    for i in Room.objects.all:
+        if i['id']==int(pk):
+            room=i
+
+    context={'room':room}
+    return render(request,'base/room.html',context)
