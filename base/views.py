45830f61d227888a42bdd7b0d75341ae3e93e6ac
ajaypatel <patel.ajay745@gmail.com>
2024-06-13 15:58:36 -0400

    Add pipular topic and funcationality

--- base/views.py
+++ base/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Count
 
 
 # Create your views here.
 
 def home(request):
-    
-    context={'rooms':Room.objects.all}
+
+    q=request.GET.get('q')
+    room=Room.objects.all()
+    if q is not None:
+        room=Room.objects.filter(topic__name__icontains=q)
+
+    pupularTopic=Topic.objects.annotate(room_count=Count('room')).order_by('-room_count')[:10]
+    context={'rooms':room,'topics':pupularTopic}    
     return render(request,'base/home.html',context)
 
 def room(request,pk):
@@ -41,7 +48,7 @@ def updateRoom(request,pk):
 def deleteRoom(request,pk):
     room=Room.objects.get(id=pk)
     if request.method=='POST':
-        Room.objects.filter(id=pk).delete()
+        room.delete()
         return redirect('home')
     context={'room':room}
     return render(request,'base/delete_room.html',context)
