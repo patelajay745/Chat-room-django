ff61cdbff1262787957dffa820da0f71686e287b
ajaypatel <patel.ajay745@gmail.com>
2024-06-14 16:11:32 -0400

    added login functionality

--- base/views.py
+++ base/views.py
@@ -2,19 +2,48 @@ from django.shortcuts import render,redirect
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Count
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 
 # Create your views here.
 
+def loginPage(request):
+    if request.method== 'POST':
+        username= request.POST.get('username')
+        password=request.POST.get('password')
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request,"user does not exists")
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else: 
+            messages.error(request,"User or Password does not exists")
+        
+    return render(request,'base/login_register.html')
+
 def home(request):
 
     q=request.GET.get('q')
-    room=Room.objects.all()
     if q is not None:
-        room=Room.objects.filter(topic__name__icontains=q)
+        room=Room.objects.filter(Q(topic__name__icontains=q) |
+                                 Q(name__icontains=q) |
+                                 Q(host__username__icontains=q) |
+                                 Q(description__icontains=q))
+    else:
+        room=Room.objects.all()
 
+    room_count=room.count() #faster then len method
+    # roomcount=len(room)
+    
     pupularTopic=Topic.objects.annotate(room_count=Count('room')).order_by('-room_count')[:10]
-    context={'rooms':room,'topics':pupularTopic}    
+    context={'rooms':room,'topics':pupularTopic,"room_count":room_count}    
     return render(request,'base/home.html',context)
 
 def room(request,pk):
