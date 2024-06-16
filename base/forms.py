07d0a8a4b51c5fa1e8bd2d93830d22eb0cd2dad7
ajaypatel <patel.ajay745@gmail.com>
2024-06-16 11:06:26 -0400

    added messages to room

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,31 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class RoomForm(ModelForm):
     class Meta:
         model=Room
         exclude = ('host',)
         fields='__all__'
+
+class RegisterForm(ModelForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, required=True, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ('first_name','last_name', 'username', 'email', 'password' )
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords do not match")
+
+        return cleaned_data
--- base/views.py
+++ base/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,redirect,HttpResponse
-from .models import Room,Topic
+from .models import *
 from django.contrib.auth.decorators import login_required
-from .forms import RoomForm
+from .forms import RoomForm,RegisterForm
 from django.db.models import Count
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login,logout
+
 
 
 # Create your views here.
@@ -42,11 +43,11 @@ def logoutUser(request):
     return redirect('home')
 
 def registerUser(request):
-    form= UserCreationForm()
+    form= RegisterForm()
     context={"form": form}
 
     if request.method=="POST":
-        form=UserCreationForm(request.POST)
+        form=RegisterForm(request.POST)
         if form.is_valid():
                 user=form.save(commit=False)
                 user.username=user.username.lower()
@@ -80,7 +81,9 @@ def home(request):
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    context={'room':Room.objects.get(id=pk)}
+    room=Room.objects.get(id=pk)
+    room_messages=room.message_set.all().order_by('-created')
+    context={'room':room,'room_messages':room_messages}
     return render(request,'base/room.html',context)
 
 @login_required(login_url='login')
