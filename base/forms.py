8e8f0d859bf6993a111ec0564afe2af8033ba760
Ajay patel <patel.ajay745@gmail.com>
2024-06-19 23:21:55 -0400

    added theme

--- base/forms.py
+++ base/forms.py
@@ -4,22 +4,30 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+
 class RoomForm(ModelForm):
     class Meta:
-        model=Room
-        exclude = ('host',)
-        fields='__all__'
+        model = Room
+        exclude = (
+            "host",
+            "participants",
+        )
+        fields = "__all__"
+
 
 class RegisterForm(ModelForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput, required=True)
-    password2 = forms.CharField(widget=forms.PasswordInput, required=True, label="Confirm Password")
+    password2 = forms.CharField(
+        widget=forms.PasswordInput, required=True, label="Confirm Password"
+    )
 
     class Meta:
         model = User
-        fields = ('first_name','last_name', 'username', 'email', 'password' )
+        fields = ("first_name", "last_name", "username", "email", "password")
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
