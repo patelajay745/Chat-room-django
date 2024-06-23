c40424ad5f678d128236c067799cacdeafae7f43
Ajay patel <patel.ajay745@gmail.com>
2024-06-22 23:59:58 -0400

    added rest api for project

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('base.urls'))
+    path("admin/", admin.site.urls),
+    path("", include("base.urls")),
+    path("api/", include("base.api.urls")),
 ]
