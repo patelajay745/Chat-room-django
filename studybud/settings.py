cd3957f626ec4c5c83e530adaa8eea2fe54edfbf
Ajay patel <patel.ajay745@gmail.com>
2024-07-25 19:30:36 -0400

    updated allowed host

--- studybud/settings.py
+++ studybud/settings.py
@@ -13,9 +13,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "studyroom.ajayproject.com"]
 
 
 # Application definition
