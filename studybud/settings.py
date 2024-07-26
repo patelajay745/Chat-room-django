dcf31f0c67ad8867d81add47f9e98dfcad759e64
Ajay patel <patel.ajay745@gmail.com>
2024-07-25 21:07:22 -0400

    updated static url in setting

--- studybud/settings.py
+++ studybud/settings.py
@@ -123,7 +123,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+if os.getenv("STATIC_ROOT"):
+    STATIC_ROOT = os.getenv("STATIC_ROOT")
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 MEDIA_URL = "/images/"
