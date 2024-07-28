ef00453d34f1943840ca89d020ecd1b7845e166b
Ajay patel <patel.ajay745@gmail.com>
2024-07-28 01:27:15 -0400

    added CSRF_TRUSTED_ORIGINS

--- studybud/settings.py
+++ studybud/settings.py
@@ -15,6 +15,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+CSRF_TRUSTED_ORIGINS = ["https://studyroom.ajayproject.com"]
+
 ALLOWED_HOSTS = ["*", "studyroom.ajayproject.com"]
 
 
